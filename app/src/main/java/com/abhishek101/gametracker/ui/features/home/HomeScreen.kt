fd60b94febf5071297ec987244d53042f89c2d59
Abhishek Dewan <adewan@umich.edu>
2021-03-17 15:50:57 -0700

    Added a bottom bar to the home screen

--- app/src/main/java/com/abhishek101/gametracker/ui/features/home/HomeScreen.kt
+++ app/src/main/java/com/abhishek101/gametracker/ui/features/home/HomeScreen.kt
@@ -1,11 +1,53 @@
 package com.abhishek101.gametracker.ui.features.home
 
+import android.content.res.Configuration
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Games
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import com.abhishek101.gametracker.ui.theme.GameTrackerTheme
 
 @Composable
 fun HomeScreen() {
-    Text("Home Screen", style = MaterialTheme.typography.h1, color = Color.White)
+    HomeScreenContent()
+}
+
+@Composable
+fun HomeScreenContent() {
+    val scaffoldState = rememberScaffoldState()
+    Scaffold(
+        scaffoldState = scaffoldState,
+        content = {
+            Text("Home Screen", style = MaterialTheme.typography.h6, color = Color.White)
+        },
+        bottomBar = {
+            BottomAppBar {
+                IconButton(modifier = Modifier.weight(1f), onClick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.Games, "Game Lists")
+                }
+                IconButton(modifier = Modifier.weight(1f), onClick = { /*TODO*/ }) {
+                    Icon(Icons.Filled.Search, "Search")
+                }
+            }
+        }
+    )
+}
+
+@Preview(uiMode = Configuration.UI_MODE_TYPE_NORMAL, device = Devices.PIXEL_4_XL)
+@Composable
+fun HomeScreenContentPreview() {
+    GameTrackerTheme {
+        HomeScreenContent()
+    }
 }
