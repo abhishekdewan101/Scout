89956afea8ae6ffe91574914b651de4c1d510295
Abhishek Dewan <adewan@umich.edu>
2021-03-27 23:57:09 -0700

    Refactored a bit and also fixed the status bar color to be dark always

--- app/src/main/java/com/abhishek101/gametracker/ui/MainActivity.kt
+++ app/src/main/java/com/abhishek101/gametracker/ui/MainActivity.kt
@@ -3,6 +3,8 @@ package com.abhishek101.gametracker.ui
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 import com.abhishek101.gametracker.ui.components.navigation.MainNavigator
 import com.abhishek101.gametracker.ui.theme.GameTrackerTheme
 
@@ -11,6 +13,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GameTrackerTheme {
+                window.statusBarColor = Color(0xFF0e0d0d).toArgb()
+                window.navigationBarColor = Color(0xFF0e0d0d).toArgb()
                 MainNavigator()
             }
         }
