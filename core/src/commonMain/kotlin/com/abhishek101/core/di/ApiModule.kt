bc2c02ad91e5c7527dc061a1f2890682c96322d8
Abhishek Dewan <adewan@umich.edu>
2021-04-07 22:48:21 -0700

    Refactored Koin into constituent modules to help with seperation of concerns

---
+++ core/src/commonMain/kotlin/com/abhishek101/core/di/ApiModule.kt
@@ -0,0 +1,44 @@
+package com.abhishek101.core.di
+
+import com.abhishek101.core.remote.GameApi
+import com.abhishek101.core.remote.GameApiImpl
+import com.abhishek101.core.remote.GenreApi
+import com.abhishek101.core.remote.GenreApiImpl
+import com.abhishek101.core.remote.PlatformApi
+import com.abhishek101.core.remote.PlatformApiImpl
+import io.ktor.client.HttpClient
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.features.logging.Logging
+import kotlinx.serialization.json.Json
+import org.koin.dsl.module
+
+val apiModule = module {
+    single<PlatformApi> {
+        PlatformApiImpl(get(), get())
+    }
+    single<GenreApi> {
+        GenreApiImpl(get(), get())
+    }
+    single<GameApi> {
+        GameApiImpl(get(), get())
+    }
+    single {
+        HttpClient {
+            val ktorLogger = get<Logger>()
+            install(JsonFeature) {
+                serializer = KotlinxSerializer(
+                    Json {
+                        ignoreUnknownKeys = true
+                    }
+                )
+            }
+            install(Logging) {
+                logger = ktorLogger
+                level = LogLevel.ALL
+            }
+        }
+    }
+}
--- core/src/commonMain/kotlin/com/abhishek101/core/di/Koin.kt
+++ core/src/commonMain/kotlin/com/abhishek101/core/di/Koin.kt
@@ -1,107 +1,20 @@
 package com.abhishek101.core.di
 
-import com.abhishek101.core.db.AppDb
-import com.abhishek101.core.remote.AuthenticationApiImpl
-import com.abhishek101.core.remote.GameApi
-import com.abhishek101.core.remote.GameApiImpl
-import com.abhishek101.core.remote.GenreApi
-import com.abhishek101.core.remote.GenreApiImpl
-import com.abhishek101.core.remote.PlatformApi
-import com.abhishek101.core.remote.PlatformApiImpl
-import com.abhishek101.core.repositories.AuthenticationRepository
-import com.abhishek101.core.repositories.AuthenticationRepositoryImpl
-import com.abhishek101.core.repositories.GameRepository
-import com.abhishek101.core.repositories.GameRepositoryImpl
-import com.abhishek101.core.repositories.GenreRepository
-import com.abhishek101.core.repositories.GenreRepositoryImpl
-import com.abhishek101.core.repositories.PlatformRepository
-import com.abhishek101.core.repositories.PlatformRepositoryImpl
-import com.abhishek101.core.utils.DatabaseHelper
-import com.abhishek101.core.utils.KtorLogger
-import com.abhishek101.core.utils.defaultAppConfig
-import io.ktor.client.HttpClient
-import io.ktor.client.features.json.JsonFeature
-import io.ktor.client.features.json.serializer.KotlinxSerializer
-import io.ktor.client.features.logging.LogLevel
-import io.ktor.client.features.logging.Logger
-import io.ktor.client.features.logging.Logging
-import kotlinx.serialization.json.Json
 import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 import org.koin.core.parameter.parametersOf
 import org.koin.core.scope.Scope
-import org.koin.dsl.module
 import kotlin.time.ExperimentalTime
 
 @ExperimentalTime
 fun initKoin(appModules: List<Module>): KoinApplication {
     val combined = appModules.toMutableList().apply {
         add(platformModule)
-        add(coreModule)
+        addAll(listOf(utilModule, dbModule, apiModule, repoModule))
     }
     return startKoin {
         modules(combined)
-    }
-}
-
-@ExperimentalTime
-val coreModule: Module = module {
-    single {
-        get<DatabaseHelper>().authenticationQueries
-    }
-    single {
-        get<DatabaseHelper>().platformQueries
-    }
-    single {
-        get<DatabaseHelper>().genreQueries
-    }
-    single {
-        DatabaseHelper(AppDb(get()))
-    }
-    single<PlatformApi> {
-        PlatformApiImpl(get(), get())
-    }
-    single<GenreApi> {
-        GenreApiImpl(get(), get())
-    }
-    single<GameApi> {
-        GameApiImpl(get(), get())
-    }
-
-    single {
-        HttpClient {
-            val ktorLogger = get<Logger>()
-            install(JsonFeature) {
-                serializer = KotlinxSerializer(
-                    Json {
-                        ignoreUnknownKeys = true
-                    }
-                )
-            }
-            install(Logging) {
-                logger = ktorLogger
-                level = LogLevel.ALL
-            }
-        }
-    }
-    single<Logger> {
-        KtorLogger(getWith("Ktor"))
-    }
-    single {
-        defaultAppConfig()
-    }
-    single<PlatformRepository> {
-        PlatformRepositoryImpl(get(), get(), get())
-    }
-    single<AuthenticationRepository> {
-        AuthenticationRepositoryImpl(AuthenticationApiImpl(get()), get())
-    }
-    single<GenreRepository> {
-        GenreRepositoryImpl(get(), get(), get())
-    }
-    single<GameRepository> {
-        GameRepositoryImpl(get(), get(), get(), get())
     }
 }
 
