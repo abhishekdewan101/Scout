250c438321005af18d2cabbbb29768d2e73cf489
Abhishek Dewan <adewan@umich.edu>
2021-04-10 22:54:00 -0700

    Android: Start of a re-usable component library that will be the design library for this project

---
+++ app/src/main/java/com/abhishek101/gamescout/design/SafeArea.kt
@@ -0,0 +1,32 @@
+package com.abhishek101.gamescout.design
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+
+/**
+ * Composable that allows you add a padding around your content without having to deal with the
+ * modifiers
+ */
+
+@Composable
+fun SafeArea(
+    padding: Dp,
+    backgroundColor: Color = MaterialTheme.colors.background,
+    content: @Composable () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .background(backgroundColor)
+            .fillMaxSize()
+            .padding(padding)
+    ) {
+        content()
+    }
+}
