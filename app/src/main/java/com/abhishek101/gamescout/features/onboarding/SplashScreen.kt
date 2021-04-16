7c4162e434627db457cc18180f89e6e5a792f395
Abhishek Dewan <adewan@umich.edu>
2021-04-16 15:37:11 -0700

    Android: New splash screen design

--- app/src/main/java/com/abhishek101/gamescout/features/onboarding/SplashScreen.kt
+++ app/src/main/java/com/abhishek101/gamescout/features/onboarding/SplashScreen.kt
@@ -1,9 +1,12 @@
 package com.abhishek101.gamescout.features.onboarding
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.CircularProgressIndicator
@@ -13,14 +16,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.navigate
 import com.abhishek101.gamescout.R
 import com.abhishek101.gamescout.theme.GameTrackerTheme
@@ -38,39 +42,54 @@ fun SplashScreen(viewModel: SplashViewModel = get()) {
 
     viewModel.checkAuthentication()
 
-    SplashScreenContent(isAuthenticationValid = isAuthenticationValid.value) {
+    if (isAuthenticationValid.value) {
         coroutineScope.launch {
             delay(1500)
             navController.popBackStack()
             navController.navigate(splashScreenDestination)
         }
+    } else {
+        SplashScreenContent()
     }
 }
 
 @Composable
-fun SplashScreenContent(isAuthenticationValid: Boolean, navigateForward: () -> Unit = {}) {
-    val image: Painter = painterResource(id = R.drawable.background)
-    Image(painter = image, contentDescription = "", modifier = Modifier.fillMaxSize())
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier
-            .fillMaxSize()
-            .semantics { testTag = "SplashScreen" }
-    ) {
-        Text(
-            "Scout",
-            style = TextStyle(color = MaterialTheme.colors.onBackground, fontSize = 48.sp)
+fun SplashScreenContent() {
+    Box(modifier = Modifier.fillMaxWidth()) {
+        val image: Painter = painterResource(id = R.drawable.background)
+        Image(
+            painter = image,
+            contentDescription = "",
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
         )
-        CircularProgressIndicator(
-            color = MaterialTheme.colors.primary,
+        Column(
             modifier = Modifier
-                .size(40.dp)
-                .padding(top = 20.dp)
-                .semantics { testTag = "loadingBar" }
-        )
-        if (isAuthenticationValid) {
-            navigateForward()
+                .fillMaxSize()
+                .background(
+                    Color(
+                        17,
+                        27,
+                        27
+                    ).copy(alpha = 0.4f)
+                ), // Fixme: Colors need to come from a theme. We need to create a material theme like thing.
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                "Scout",
+                style = MaterialTheme.typography.h2.copy(
+                    color = Color(27, 127, 254),
+                    fontWeight = FontWeight.Bold
+                )
+            )
+            CircularProgressIndicator(
+                color = Color.White,
+                modifier = Modifier
+                    .size(40.dp)
+                    .padding(top = 20.dp)
+                    .semantics { testTag = "loadingBar" }
+            )
         }
     }
 }
@@ -79,6 +98,6 @@ fun SplashScreenContent(isAuthenticationValid: Boolean, navigateForward: () -> U
 @Composable
 fun Splash_Screen_Invalid_Authentication_Preview() {
     GameTrackerTheme {
-        SplashScreenContent(isAuthenticationValid = false)
+        SplashScreenContent()
     }
 }
