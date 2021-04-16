daa4a1b9dcfca4befcf89c6490f82184f86aae8b
Abhishek Dewan <adewan@umich.edu>
2021-04-15 23:16:31 -0700

    Android: Added a bottom padding override in safe areas for scenarios like the home screen where the bottom navigation tab needs a bigger padding value

--- app/src/main/java/com/abhishek101/gamescout/design/SafeArea.kt
+++ app/src/main/java/com/abhishek101/gamescout/design/SafeArea.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 /**
  * Composable that allows you add a padding around your content without having to deal with the
@@ -19,14 +20,28 @@ import androidx.compose.ui.unit.Dp
 fun SafeArea(
     padding: Dp,
     backgroundColor: Color = MaterialTheme.colors.background,
+    bottomOverride: Dp? = 0.dp,
     content: @Composable () -> Unit
 ) {
     Box(
         modifier = Modifier
             .background(backgroundColor)
             .fillMaxSize()
-            .padding(padding)
+            .then(getPadding(padding, bottomOverride))
     ) {
         content()
+    }
+}
+
+private fun getPadding(padding: Dp, bottomOverride: Dp?): Modifier {
+    return if (bottomOverride != null) {
+        Modifier.padding(
+            start = padding,
+            top = padding,
+            bottom = bottomOverride,
+            end = padding
+        )
+    } else {
+        Modifier.padding(padding)
     }
 }
