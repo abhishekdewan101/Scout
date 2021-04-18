793000913469117476de3ce24436fcb19a859470
Abhishek Dewan <adewan@umich.edu>
2021-04-17 18:33:55 -0700

    Android: Got emojies for everything

--- app/src/main/java/com/abhishek101/gamescout/features/genreselection/GenreSelectionViewModel.kt
+++ app/src/main/java/com/abhishek101/gamescout/features/genreselection/GenreSelectionViewModel.kt
@@ -8,6 +8,32 @@ import com.abhishek101.core.repositories.GenreRepository
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
+val genreEmojiMap = mapOf(
+    "point-and-click" to "🖱",
+    "fighting" to "🤜 🤛",
+    "shooter" to "🔫",
+    "music" to "🎤🎸",
+    "platform" to "🧗",
+    "puzzle" to "🧩",
+    "racing" to "🏎",
+    "role-playing-rpg" to "👮👨‍🍳‍",
+    "real-time-strategy-rts" to "🤔",
+    "moba" to "🖥😱",
+    "card-and-board-game" to "🃏",
+    "visual-novel" to "📚👀",
+    "arcade" to "🕹",
+    "indie" to "😍🕹",
+    "adventure" to "🤠",
+    "pinball" to "👾",
+    "quiz-trivia" to "🧐",
+    "hack-and-slash-beat-em-up" to "⚔️",
+    "tactical" to "🎲",
+    "turn-based-strategy-tbs" to "🙋🙋‍‍",
+    "strategy" to "♟♗",
+    "sport" to "⚽️",
+    "simulator" to "📺"
+)
+
 class GenreSelectionViewModel(private val genreRepository: GenreRepository) : ViewModel() {
     val genres = mutableStateOf(listOf<Genre>())
     val isLoading = mutableStateOf(true)
