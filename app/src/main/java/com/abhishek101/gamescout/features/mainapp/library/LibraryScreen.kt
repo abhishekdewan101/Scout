1bab47464b94a92c584ff59fd2bdee073a496516
Abhishek Dewan <adewan@umich.edu>
2021-05-30 21:59:48 -0700

    Android: Fixed some UI bug where the title wasn't bold

--- app/src/main/java/com/abhishek101/gamescout/features/mainapp/library/LibraryScreen.kt
+++ app/src/main/java/com/abhishek101/gamescout/features/mainapp/library/LibraryScreen.kt
@@ -1,5 +1,6 @@
 package com.abhishek101.gamescout.features.mainapp.library
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
@@ -11,8 +12,10 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.abhishek101.gamescout.design.SafeArea
+import com.abhishek101.gamescout.design.SearchAppBar
 import org.koin.androidx.compose.get
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun LibraryScreen(viewModel: LibraryViewModel = get(), navigate: (String) -> Unit) {
 
@@ -21,61 +24,11 @@ fun LibraryScreen(viewModel: LibraryViewModel = get(), navigate: (String) -> Uni
             modifier = Modifier
                 .fillMaxSize()
         ) {
-            item {
-                Text(
-                    "Library",
-                    style = MaterialTheme.typography.h4.copy(
-                        color = MaterialTheme.colors.onBackground,
-                        fontWeight = FontWeight.Bold
-                    ),
-                    modifier = Modifier.pointerInput(Unit) {
-                        detectTapGestures(
-                            onLongPress = {
-                                viewModel.deleteAllGames()
-                            }
-                        )
-                    }
-                )
+            stickyHeader {
+                SearchAppBar(title = "Library", placeholderText = "Search library") {
+                    
+                }
             }
-
-            // item {
-            //     val libraryGames = viewModel.libraryGames.value
-            //     if (libraryGames != null) {
-            //         val wishList = libraryGames.filter { it.status == LibraryGameStatus.WISHLISTED }
-            //         val listData = wishList.map { it.coverUrl }
-            //         SafeArea(padding = 0.dp, topOverride = 10.dp) {
-            //             TitleContainer(title = "WishList", hasViewMore = false) {
-            //                 GridImageList(
-            //                     data = listData,
-            //                     columns = 3,
-            //                     imageWidth = 125.dp,
-            //                     imageHeight = 175.dp
-            //                 ) {
-            //                     navigate("${MainAppDestinations.GameDetail.name}/${wishList[it].slug}")
-            //                 }
-            //             }
-            //         }
-            //     }
-            // }
-            //
-            // item {
-            //     val libraryGames = viewModel.libraryGames.value
-            //     if (libraryGames != null) {
-            //         val listData = libraryGames.map { it.coverUrl }
-            //         SafeArea(padding = 0.dp, topOverride = 10.dp) {
-            //             TitleContainer(title = "All Games", hasViewMore = false) {
-            //                 GridImageList(
-            //                     data = listData,
-            //                     columns = 3,
-            //                     imageWidth = 125.dp,
-            //                     imageHeight = 175.dp
-            //                 ) {
-            //                     navigate("${MainAppDestinations.GameDetail.name}/${libraryGames[it].slug}")
-            //                 }
-            //             }
-            //         }
-            //     }
-            // }
         }
     }
 }
