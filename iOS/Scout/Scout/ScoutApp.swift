3775b615238912f619da3aff16750fdbd336786a
Abhishek Dewan <adewan@umich.edu>
2021-06-19 18:43:07 -0700

    iOS - Created a new empty iOS project for Scout

---
+++ iOS/Scout/Scout/ScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutApp.swift
+//  Scout
+//
+//  Created by Abhishek Dewan on 6/19/21.
+//
+
+import SwiftUI
+
+@main
+struct ScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
