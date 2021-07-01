e78eb0780ea32a47bfd2e5c7afabf539fe1ef6f2
Abhishek Dewan <adewan@umich.edu>
2021-07-01 00:22:02 -0700

    iOS - More work done on the game detail page
    Game detail page now has an expandable summary

--- iOS/Scout/Scout/Features/GameDetails/GameDetailScreen.swift
+++ iOS/Scout/Scout/Features/GameDetails/GameDetailScreen.swift
@@ -35,39 +35,21 @@ struct GameDetailScreen: View {
             let result = viewState as! GameDetailViewState.NonEmptyViewState
             ScrollView(.vertical, showsIndicators: false) {
                 VStack {
-                    HStack {
-                        AsyncImage(url: result.coverUrl,
-                                   width: 175,
-                                   height: 225,
-                                   contentMode: .fill,
-                                   cornerRadius: 5)
-                        VStack(alignment: .leading, spacing: 10) {
-                            Text(result.name).font(.title2).fontWeight(.bold).lineLimit(2)
-                            if let developer = result.developer {
-                                Text(developer.name).font(.body).foregroundColor(Color("White").opacity(0.7))
-                            }
-                            Text(result.releaseDate.dateString).font(.body).foregroundColor(Color("White").opacity(0.7))
-                            if let rating = result.rating {
-                                ZStack {
-                                    Circle().fill(Color.green).frame(width: 45, height: 45)
-                                    Text("\(rating)").font(.body).fontWeight(.bold)
-                                }
-                            }
-                        }
-                        .frame(alignment: .topLeading)
-                        .padding(.leading)
+                    GameDetailHeaderView(result: result)
+                    if let summary = result.summary {
+                        GameDetailSummary(summary: summary)
                     }
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.top)
-                .padding(.horizontal)
-            }.navigationBarTitle(result.name)
-            .navigationBarTitleDisplayMode(.inline)
+                }.frame(maxWidth: .infinity, alignment: .leading)
+            }.navigationBarTitle(result.name, displayMode: .inline)
             .toolbar {
-                Button {
-                    print("save game")
-                } label: {
-                    Image(systemName: "heart")
+                // Fix https://stackoverflow.com/questions/64405106/toolbar-is-deleting-my-back-button-in-the-navigationview/64994154#64994154
+                ToolbarItem(placement: .navigationBarLeading) {Text("")}
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        print("Save Game")
+                    } label: {
+                        Image(systemName: "heart").font(.body)
+                    }
                 }
             }
         }
@@ -77,5 +59,69 @@ struct GameDetailScreen: View {
 struct GameDetailScreen_Previews: PreviewProvider {
     static var previews: some View {
         GameDetailScreen(slug: "Something")
+    }
+}
+
+struct GameDetailSummary: View {
+    var summary: String
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text("About")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color("White").opacity(0.5))
+            ExpandableTextField(text: summary)
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.top)
+    }
+}
+
+struct GameDetailHeaderView: View {
+    var result: GameDetailViewState.NonEmptyViewState
+
+    var body: some View {
+        HStack {
+            AsyncImage(url: result.coverUrl,
+                       width: 175,
+                       height: 225,
+                       contentMode: .fill,
+                       cornerRadius: 5)
+            VStack(alignment: .leading, spacing: 10) {
+                Text(result.name).font(.title2).fontWeight(.bold).lineLimit(2)
+                if let developer = result.developer {
+                    Text(developer.name).font(.body).foregroundColor(Color("White").opacity(0.7))
+                }
+                Text(result.releaseDate.dateString).font(.body).foregroundColor(Color("White").opacity(0.7))
+                if let rating = result.rating {
+                    RatingView(rating: rating.intValue)
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.leading)
+        }
+    }
+}
+
+struct RatingView: View {
+    var rating: Int
+
+    func getRatingColor(rating: Int) -> Color {
+        switch rating {
+        case 75...100: return Color.green
+        case 55...74: return Color.yellow
+        case 0...74: return Color.red
+        default: return Color.blue
+        }
+    }
+
+    var body: some View {
+        ZStack {
+            Circle()
+                .fill(getRatingColor(rating: rating))
+                .frame(width: 45, height: 45)
+            Text(String(rating)).font(.body).fontWeight(.bold)
+        }
     }
 }
