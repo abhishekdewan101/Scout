a0980db29b70db937fa029bc81c89890051dd664
Abhishek Dewan <adewan@umich.edu>
2021-07-12 23:01:57 -0700

    Android: Removing the old code in favor of new one

--- app/src/main/java/com/abhishek101/gamescout/features/preferenceselection/GenreSelectionScreen.kt
+++ app/src/main/java/com/abhishek101/gamescout/features/preferenceselection/GenreSelectionScreen.kt
@@ -30,11 +30,37 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.abhishek101.core.viewmodels.preferenceselection.PreferenceSelectionViewModel
 import com.abhishek101.core.viewmodels.preferenceselection.PreferenceSelectionViewState
+import com.abhishek101.gamescout.R
 import com.abhishek101.gamescout.design.new.image.SelectableRemoteImage
 import com.abhishek101.gamescout.design.new.system.SystemUiControlView
-import com.abhishek101.gamescout.features.genreselection.genreImageMap
 import com.abhishek101.gamescout.theme.ScoutTheme
 import org.koin.androidx.compose.get
+
+val genreImageMap = mapOf(
+    "point-and-click" to R.drawable.point_click,
+    "fighting" to R.drawable.fighting,
+    "shooter" to R.drawable.shooter,
+    "music" to R.drawable.music,
+    "platform" to R.drawable.platformer,
+    "puzzle" to R.drawable.puzzle,
+    "racing" to R.drawable.racing,
+    "role-playing-rpg" to R.drawable.role_playing_game,
+    "real-time-strategy-rts" to R.drawable.rts,
+    "moba" to R.drawable.moba,
+    "card-and-board-game" to R.drawable.card_board_games,
+    "visual-novel" to R.drawable.visual_novel,
+    "arcade" to R.drawable.ic_arcade,
+    "indie" to R.drawable.indie_games,
+    "adventure" to R.drawable.ic_adventure,
+    "pinball" to R.drawable.pinball,
+    "quiz-trivia" to R.drawable.quiz_trivia,
+    "hack-and-slash-beat-em-up" to R.drawable.hack_slash,
+    "tactical" to R.drawable.tactical,
+    "turn-based-strategy-tbs" to R.drawable.turn_based,
+    "strategy" to R.drawable.stratergy,
+    "sport" to R.drawable.sports,
+    "simulator" to R.drawable.simulator
+)
 
 @Composable
 fun GenreSelectionScreen(
