15d4e7b55266ba1de13f77df3369323134254c6b
Abhishek Dewan <adewan@umich.edu>
2021-07-18 00:10:38 -0700

    Android: Added scaffolds for Search and Collections

--- app/src/main/java/com/abhishek101/gamescout/features/main/search/SearchTab.kt
+++ app/src/main/java/com/abhishek101/gamescout/features/main/search/SearchTab.kt
@@ -1,20 +1,35 @@
 package com.abhishek101.gamescout.features.main.search
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import com.abhishek101.gamescout.theme.ScoutTheme
 
 @Composable
 fun SearchTab() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(ScoutTheme.colors.secondaryBackground)
-    ) {
-        Text("Search Tab", color = ScoutTheme.colors.textOnSecondaryBackground)
-    }
+    val scaffoldState = rememberScaffoldState()
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = { SearchTopBar() },
+        content = {
+        },
+        backgroundColor = ScoutTheme.colors.secondaryBackground
+    )
+}
+
+@Composable
+private fun SearchTopBar() {
+    TopAppBar(
+        backgroundColor = ScoutTheme.colors.topBarBackground,
+        title = {
+            Text(
+                text = "Search",
+                fontWeight = FontWeight.Bold,
+                color = ScoutTheme.colors.topBarTextColor
+            )
+        }
+    )
 }
