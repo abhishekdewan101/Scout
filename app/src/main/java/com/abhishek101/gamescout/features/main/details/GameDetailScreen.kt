0c5bcd8bab5c1d3d3bdc4c3737563e299efb9042
Abhishek Dewan <adewan@umich.edu>
2021-07-21 22:38:21 -0700

    Android: Deleted some old unused code

--- app/src/main/java/com/abhishek101/gamescout/features/main/details/GameDetailScreen.kt
+++ app/src/main/java/com/abhishek101/gamescout/features/main/details/GameDetailScreen.kt
@@ -1,9 +1,22 @@
 package com.abhishek101.gamescout.features.main.details
 
-import androidx.compose.material.Text
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import com.abhishek101.gamescout.theme.ScoutTheme
 
 @Composable
 fun GameDetailScreen(data: String) {
-    Text(data)
+    val scaffoldState = rememberScaffoldState()
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = { GameDetailTopBar() },
+        content = {},
+        backgroundColor = ScoutTheme.colors.secondaryBackground
+    )
+}
+
+@Composable
+private fun GameDetailTopBar() {
 }
